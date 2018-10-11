8c9826a0d82512ac8fa56ff2030f89d8e123f69a
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:22:46 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -2,7 +2,8 @@
 x <- USArrests
 #cbind() function takes a sequence of dataframe and combines by rows or columns
 x=cbind(rownames(x),x)
-colnames(x)=c("stateName")
+#the above function copies the rownames and places it as a column in the existing dataframe 
+colnames(x)=c("stateName")#Giving name to the new column
 mergeddataframe=merge(x,clean,by="stateName")
 colnames(mergeddataframe)=c("stateName","Murder","Assault","UrbanPop","Rape","population","popOver18","percentOver18")
 mergeddataframe
