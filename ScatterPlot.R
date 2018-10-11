4974b84a7d51f5b0e748c3b932a147882b1642fd
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:25:25 -0400

    Saving ScatterPlot.R

--- ScatterPlot.R
+++ ScatterPlot.R
@@ -1,6 +1,6 @@
 
 library(ggplot2)
-# 12)	 Generate a scatter plot - have population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
+#Generate a scatter plot - have population on the X axis, the percent over 18 on the y axis, and the size & color represent the murder rate
 
-ggplot(mergeddataframe,aes(x=population,y=percentOver18)) + geom_point(aes(size=Murder,color=Murder))
+p <- ggplot(mergeddataframe,aes(x=population,y=percentOver18)) + geom_point(aes(size=Murder,color=Murder))
 #geom_point() function is used to create scatter plots. Scatter plot is most useful for displaying relationship between two continuous variables.
