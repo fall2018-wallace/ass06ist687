3f12a4ff67989de3836b7a7d746dcc19eaab8f8e
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 13:14:04 -0400

    Saving BarChart.R

--- BarChart.R
+++ BarChart.R
@@ -1,7 +1,10 @@
 
+library(ggplot2)
 #8:Generate a bar chart, with the number of murders per state
 ggplot(mergeddataframe,aes(x=stateName,y=Murder)) + geom_col()
 
+
+#9:Generate a bar chart, with the number of murders per state. Rotate text (on the X axis), so we can see x labels, also add a title named “Total Murders”.
 ggplot(mergeddataframe,aes(x=stateName,y=Murder)) + geom_col() + theme(axis.text.x=element_text(angle=90,hjust=1)) + ggtitle("Total Murders")
 #theme() function allows you to control the appearance of all non-data components
 #ggtitle() funtion is used to give title to the ggplot diagram
