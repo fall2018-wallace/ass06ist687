31444417d9fd747d31716151104fda2b4b79b156
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:30:33 -0400

    Saving Merge.R

--- Merge.R
+++ Merge.R
@@ -3,3 +3,5 @@ x <- USArrests
 x=cbind(rownames(x),x)
 colnames(x)=c("stateName")
 mergeddataframe=merge(x,newframe,by="stateName")
+colnames(mergeddataframe)=c("stateName","Murder","Assault","UrbanPop","Rape","population","popOver18","percentOver18")
+mergeddataframe
