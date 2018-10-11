0620f9c7e06b590eeb3a35a99fe97721fb736fea
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:43:08 -0400

    Saving Histogram.R

--- Histogram.R
+++ Histogram.R
@@ -1,1 +1,10 @@
 
+ggplot(mergeddataframe,aes(x=population))+geom_histogram(bins = 50, color = "black", fill = "black")
+
+ggplot(mergeddataframe,aes(x=Murder))+geom_histogram(bins = 50, color = "blue", fill = "blue")
+
+ggplot(mergeddataframe,aes(x=Assault))+geom_histogram(bins = 50, color = "red", fill = "red")
+
+ggplot(mergeddataframe,aes(x=UrbanPop))+geom_histogram(bins = 50, color = "green", fill = "green")
+
+ggplot(mergeddataframe,aes(x=Rape))+geom_histogram(bins = 50, color = "yellow", fill = "yellow")
