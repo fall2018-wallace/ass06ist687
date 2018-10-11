3da715aae96dde2f0213788e02df9e8b94a5fac9
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 12:23:44 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,22 +1,3 @@
 
-clean_data <- raw_data
-readStates <- function(states)
-{
-    # remove rows that not needed
-    # -- first row is the total for the US, we do not need that
-    states <- states[-1,]
-    # -- last row is Puerto Rico, it's not a state 
-    num.row <- nrow(states)
-    states <- states[-num.row,]
-    
-    # remove the first 4 columns
-    states <- states[,-1:-4]
-    
-    colnames(states) <- c("stateName","population","popOver18","percentOver18")
-    
-    # return the results 
-    return(states)
-}
-
-cleanCensus <- readStates(raw_data)
-str(cleanCensus)
+newframe <- dfStates[ , -1:-4]
+newframe <- newframe[-c(1,53),]
