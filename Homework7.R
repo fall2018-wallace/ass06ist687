abcebd71d164790ccc6db234caaf747b91bac90f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 01:58:34 -0400

    Saving Homework7.R

--- Homework7.R
+++ Homework7.R
@@ -3,3 +3,20 @@
 
 library(maps)
 library(ggplot2)
+
+#Add the area of each state, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.area’ and ‘state.name’ vectors
+
+stateArea <- state.area
+centerOfState <- state.center
+stateName <- state.name
+
+#data.frame() function creates a dataframe by coupling the variables
+df <- data.frame(stateName,stateArea,centerOfState)
+View(df)
+#merging the new created dataframe with the previous step mergeddataframe by variable "stateName"
+mergeddataframe <- merge(mergedataframe,df, by="stateName")
+View(mergeddataframe)
+#tolower() function converts the characters in character variable to lower case
+#we are using tolower() function because ggplot wants all the stateNames to be in lower case
+mergeddataframe$stateName <- tolower(mergeddataframe$stateName)
+View(mergeddataframe)
